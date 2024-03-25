f682daac2cb839f6c40630b9a40ea1c8cc60101b
priya-jan <priyanshijain1628@gmail.com>
2024-03-26 02:40:55 +0530

    2.40 am

--- requirements.py
+++ requirements.py
@@ -1,7 +1,7 @@
 transformers
-transformers[sentencepieces] 
+transformers[sentencepiece] 
 datasets
-sacreblue:-evolution matrix for text summarization we use row matrix 
+sacrebleu
 rouge_score
 py7zr
 pandas
@@ -9,14 +9,14 @@ nltk
 tqdm
 PyYAML
 matplotlib
-torch\
+torch
 notebook
 boto3
 mypy-boto3-s3
 python-box==6.0.2
 ensure==1.0.2
 fastapi==0.78.0
-unicorn==0.18.3
+uvicorn==0.18.3
 Jinja2==3.1.2
 
 -e .
--- setup.py
+++ setup.py
@@ -2,3 +2,26 @@ import setuptools
 
 with open("README.md", "r",encoding="utf-8") as f:
     long_description = f.read()
+
+__version__ ="0.0.0"
+REPO_NAME="text_summarizer_project"
+AUTHOR_USER_NAME="priya-jan"
+SRC_REPO="textSummarizer"
+AUTHOR_EMAIL="priyanshijain1628@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A samll python package for NLP app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+        
+    },
+    package_dir={"":"src"},
+    packages=setuptools.find_packages(where="src")
+)
